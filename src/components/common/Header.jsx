151cb2b5bd95b118a51efc6f620a77555178fceb
Sana Ahmad <sanaahmad17771@gmail.com>
2025-07-24 15:46:16 +0500

    sticky header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,80 +5,49 @@ import { RiFacebookFill } from "react-icons/ri";
 import { FaXTwitter } from "react-icons/fa6";
 import { LuInstagram } from "react-icons/lu";
 import { RxLinkedinLogo } from "react-icons/rx";
+
 const Header = () => {
   return (
-    <div>
-      <div className="bg-[#1E3D69] hidden  sm:flex justify-center lg:justify-between items-center h-12 md:px-40  text-[16px] font-normal">
-        {/*  */}
-        <div className="flex items-center text-white space-x-4 ">
+    <div className="sticky top-0 z-50 w-full bg-[#F5F5F5] shadow-md">
+      {/* Top Bar */}
+      <div className="bg-[#1E3D69] hidden sm:flex justify-center lg:justify-between items-center h-12 md:px-40 text-[16px] font-normal">
+        <div className="flex items-center text-white space-x-4">
           <div className="w-[1.5px] h-12 bg-[rgba(255,255,255,0.20)]"></div>
-          <span>
-            <FiPhone />
-          </span>
-          <span>
-            (239) 555-0108
-          </span>
+          <span><FiPhone /></span>
+          <span>(239) 555-0108</span>
           <div className="w-[1.5px] h-12 bg-[rgba(255,255,255,0.20)]"></div>
-          <span>
-            <MdOutlineEmail />
-          </span>
-          <span>
-            info@safecareind.xyz
-          </span>
+          <span><MdOutlineEmail /></span>
+          <span>info@safecareind.xyz</span>
           <div className="w-[1.5px] h-12 bg-[rgba(255,255,255,0.20)]"></div>
         </div>
-        {/*  */}
-        <div className="hidden  lg:flex items-center space-x-4">
+
+        <div className="hidden lg:flex items-center space-x-4">
           <div className="w-[1.5px] h-12 bg-[rgba(255,255,255,0.20)]"></div>
-          <span>
-            <a href="">
-              <RiFacebookFill color="white"/>
-            </a>
-          </span>
+          <a href="#"><RiFacebookFill color="white" /></a>
           <div className="w-[1.5px] h-12 bg-[rgba(255,255,255,0.20)]"></div>
-          <span>
-            <a href="">
-              <FaXTwitter color="white"/>
-            </a>
-          </span>
+          <a href="#"><FaXTwitter color="white" /></a>
           <div className="w-[1.5px] h-12 bg-[rgba(255,255,255,0.20)]"></div>
-          <span>
-            <a href="">
-              <LuInstagram  color="white"/>
-            </a>
-          </span>
+          <a href="#"><LuInstagram color="white" /></a>
           <div className="w-[1.5px] h-12 bg-[rgba(255,255,255,0.20)]"></div>
-          <span>
-            <a href=""><RxLinkedinLogo color="white" /></a>
-          </span>
+          <a href="#"><RxLinkedinLogo color="white" /></a>
           <div className="w-[1.5px] h-12 bg-[rgba(255,255,255,0.20)]"></div>
         </div>
       </div>
-      {/* logo section */}
-      <div className="flex justify-between items-center px-5 !lg:pr-[478px]   xl:mx-40 bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.05)]">
+
+      {/* Navbar */}
+      <div className="flex justify-between items-center px-5 !lg:pr-[478px] xl:mx-40 bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.05)]">
         <div className="flex-shrink-0">
           <img src={Logo} alt="logo" />
         </div>
         <div className="hidden lg:block">
-          <ul className="flex justify-center items-center !font-poppins space-x-10 list-none decoration-none font-normal text-[16px]">
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#about">About Us</a>
-            </li>
-            <li>
-              <a href="#products">Products</a>
-            </li>
-            <li>
-              <a href="#gallery">Gallery</a>
-            </li>
-            <li>
-              <a href="#career">Careers</a>
-            </li>
+          <ul className="flex justify-center items-center font-poppins space-x-10 list-none font-normal text-[16px]">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#products">Products</a></li>
+            <li><a href="#gallery">Gallery</a></li>
+            <li><a href="#career">Careers</a></li>
           </ul>
         </div>
-        {/* Mobile Menu*/}
         <div className="lg:hidden">
           <button className="text-[#1E3D69] focus:outline-none">☰</button>
         </div>
