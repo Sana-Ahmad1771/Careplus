27597560cc2d7dfbd51d208a7ce59e46e262bd90
Sana Ahmad <sanaahmad17771@gmail.com>
2025-08-04 12:14:29 +0500

    added scroll behaviour to auto when go to next page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
-import Home from "./home/page.jsx"
+import ScrollToTop from "./components/common/ScrollToTop.jsx";
+
+import Home from "./home/page.jsx";
 import About from "./about/page.jsx";
-import Gallery from "./gallery/page.jsx"
+import Gallery from "./gallery/page.jsx";
 import Contact from "./contact/page.jsx";
 import Products from "./products/page.jsx";
-
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Home/>}  />
-        <Route path="/about" element={<About/>}  />
-        <Route path="/products" element={<Products/>}  />
-        <Route path="/gallery" element={<Gallery/>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
 }
+
 export default App;
